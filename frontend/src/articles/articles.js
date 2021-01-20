b9f7dd932ad450f93c8289d23e4e8af01a61010f
jpec57 <jean.paul.bella@hotmail.fr>
2021-01-20 21:37:10 +0100

    Remove useless article

--- frontend/src/articles/articles.js
+++ frontend/src/articles/articles.js
@@ -3,6 +3,5 @@ import flutter_intro from '../assets/articles/flutter_intro.md'
 export default [
 
     new Article("Pourquoi j'estime que Flutter est le meilleur choix actuel pour le développement mobile", "Pourquoi choisir Flutter", flutter_intro),
-    new Article("Article 2", "Un second titre", "# TOTO JE SUIS LE CONTENU"),
 
 ];
