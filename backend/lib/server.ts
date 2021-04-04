f48de6b3e51bd8a6dd0ed57c9fa1e489709b7930
jpec57 <jean.paul.bella@hotmail.fr>
2021-04-04 21:15:22 +0200

    For server

--- backend/lib/server.ts
+++ backend/lib/server.ts
@@ -1,4 +1,4 @@
 import app from "./app";
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8002;
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
