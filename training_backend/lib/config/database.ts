ed7d8b7e4678aec9d785f9c9270606bbfd2792fe
Jpec57 <jean.paul.bella@hotmail.fr>
2021-04-07 13:50:56 +0200

    Nodenev

--- training_backend/lib/config/database.ts
+++ training_backend/lib/config/database.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config();
 
-export const database = new Sequelize('jpec_home_training', 'root', 'root', {
+export const database = new Sequelize('jpec_home_training', process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
   storage: './database.sqlite'
--- training_backend/lib/server.ts
+++ training_backend/lib/server.ts
@@ -1,4 +1,5 @@
 import app from "./app";
+import dotenv from 'dotenv';
+dotenv.config();
 const PORT = process.env.PORT || 3001;
-
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
